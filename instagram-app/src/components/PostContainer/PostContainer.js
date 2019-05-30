642b3ff01f49361c0e4e3b828ddc84eb156719bd
Shobhita Vaishnav <to.shobhita@gmail.com>
2019-05-30 10:10:21 -0700

    Day II MVP complete

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -5,30 +5,62 @@ import './PostContainer.css'
 const img_heart = require('./icons8-heart-outline-100.png');
 const img_bubble = require('./icons8-topic-100.png');
 
+class PostContainer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {comment: ""};
+    }
 
-const PostContainer = props => {
-    return (
-        <div>
-            <div className="post-div">
-                <div className="poster">
-                    <div className="image-cropper">
-                        <img className="logo" src={props.post.thumbnailUrl} alt=""/>
+    changeInputHandler = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.addNewComment(this.props.post.id, this.state.comment);
+        this.setState({ comment: "" });
+    };
+
+    handleClick = event => {
+        event.preventDefault();
+        this.props.incrementLikes(this.props.post.id);
+    };
+    
+    render() {
+        return (
+            <div>
+                <div className="post-div">
+                    <div className="poster">
+                        <div className="image-cropper">
+                            <img className="logo" src={this.props.post.thumbnailUrl} alt=""/>
+                        </div>
+                        <h4 className="poster-name">{this.props.post.username}</h4>
                     </div>
-                    <h4 className="poster-name">{props.post.username}</h4>
+
+                    <img className="post-img" src={this.props.post.imageUrl} alt=""/>
+                    <div className="heart-and-bubble">
+                        <img src={img_heart}  onClick={this.handleClick}  className="img-heart1" alt=""/>
+                        <img src={img_bubble} className="img-bubble" alt=""/>
+                    </div>
+                    <p>
+                        <strong>{this.props.post.likes} likes</strong>
+                    </p>
+                    <CommentSection comments={this.props.post.comments}/>
+                    <p>{this.props.post.timestamp}</p>
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text" 
+                            name="comment"
+                            value={this.state.comment}
+                            placeholder="Add a comment..."
+                            id="new-comment"
+                            onChange={this.changeInputHandler}
+                            className="comment-input" />
+                    </form>
                 </div>
-                
-                <img className="post-img" src={props.post.imageUrl} alt=""/>
-                <div className="heart-and-bubble" >
-                    <img src={img_heart} className="img-heart1" alt="" />
-                    <img src={img_bubble} className="img-bubble" alt="" />
-                </div>
-                <p><strong>{props.post.likes} likes</strong></p>
-                <CommentSection comments={props.post.comments}/>
-                <p>{props.post.timestamp}</p>
-                <input className="comment-input" type="text" placeholder="Add a comment..."/>
             </div>
-        </div>
-    )
+        )
+    }
 };
 
 export default PostContainer;
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -7,27 +7,48 @@ const img_compass = require('./icons8-compass-48.png');
 const img_heart = require('./icons8-heart-outline-100.png');
 const img_profile = require('./icons8-customer-32.png');
 
-const SearchBar=()=>{
-return (
-// <div className="searchbardiv" style={{ display: "flex", flexDirection: "row", background :"dodgerblue" }}></div>
-<div className="searchbar">
-    <div className="left">
-        <img src={img_logo} className="img-logo" alt=""/>
-        <div class="vertical-line"/>
-        <img src={img_label} className="img-label" alt=""/>
-    </div>
-    <div className="middle">
-        <form className="search-form" action="<tbd>">
-            <input type="text" className="search-input" name="search-input" placeholder="Search"/>
-        </form>
-    </div>
-    <div className="right">
-        <img src={img_compass} className="img-compass" alt="" />
-        <img src={img_heart} className="img-heart" alt="" />
-        <img src={img_profile} className="img-profile" alt=""/>
-    </div>
-</div>
-);
+class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {searchinput: ""};
+    }
 
+    changeInputHandler = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.searchByPosterId(this.state.searchinput);
+        this.setState({ searchinput: "" });
+    };
+
+    render() {
+        return (
+            <div className="searchbar">
+                <div className="left">
+                    <img src={img_logo} className="img-logo" alt=""/>
+                    <div className="vertical-line"/>
+                    <img src={img_label} className="img-label" alt=""/>
+                </div>
+                <div className="middle">
+                    <form className="search-form" onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text" 
+                            className="search-input" 
+                            name="searchinput" 
+                            placeholder="Search" 
+                            onChange={this.changeInputHandler}/>
+                    </form>
+                </div>
+                <div className="right">
+                    <img src={img_compass} className="img-compass" alt="" />
+                    <img src={img_heart} className="img-heart" alt="" />
+                    <img src={img_profile} className="img-profile" alt=""/>
+                </div>
+            </div>
+        );        
+    }
 };
+
 export default SearchBar;
